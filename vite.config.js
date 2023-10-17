67c6b41e39d85b9568ff82e5dbb5caa041b5ded5
yumubi <shaman2077c@163.com>
2023-10-17 23:20:46 +0800

    :tada: hover card

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Component from 'unplugin-vue-components/vite'
+import RadixVueResolver from 'radix-vue/resolver'
+import { fileURLToPath, URL } from "node:url";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Component({
+      dts: true,
+      resolvers: [
+        RadixVueResolver()
+
+        // RadixVueResolver({
+        //   prefix: '' // use the prefix option to add Prefix to the imported components
+        // })
+      ],
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"]
+  },
+})
